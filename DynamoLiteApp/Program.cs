a9fa4922ace1bf1be74726e667f72d2923ac316a
Ryan <ryanri@magenic.com>
2015-10-16 11:24:09 -0500

    just playing around.  probably abondoning this CSProj

--- DynamoLiteApp/Program.cs
+++ DynamoLiteApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DynamoLiteApp
 {
@@ -10,6 +11,35 @@ namespace DynamoLiteApp
     {
         static void Main(string[] args)
         {
+            var theTable = new SurfaceArea();
+            theTable.Show();
+
+            var stayAlive = true;
+            do
+            {
+                Console.WriteLine("<==== waiting for next command.");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "LIST":
+                        Console.WriteLine("LIST command recieved.");
+                        break;
+                    case "xXx":
+                        Console.WriteLine("exit command recieved.  please press <enter> to continue;");
+                        Console.ReadLine();
+                        stayAlive = false;
+                        break;
+                    default:
+                        
+                        break;
+                }
+
+
+            } while (stayAlive);
+
         }
+
+
+
     }
 }
